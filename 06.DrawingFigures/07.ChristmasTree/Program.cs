0f1dc9caf86f0cc39643b4ae6c98e0eed9824401
Someone Else <medical_boy_sf@yahoo.com>
2018-04-25 22:01:38 +0300

    Christmas Tree and Sunglasses Exercises

---
+++ 06.DrawingFigures/07.ChristmasTree/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _07.ChristmasTree
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = inputMethod();
+
+            Console.WriteLine(new String(' ', n + 1) + "|");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(new String(' ', n - i) + new String('*', i) + " | " + new String('*', i));
+            }
+        }
+
+        private static int inputMethod()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ 06.DrawingFigures/08.Sunglasses/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.Sunglasses
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine(new String('*', 2 * n) + new String(' ', n) + new String('*', 2 * n));
+
+            for (int i = 1; i <= n - 2; i++)
+            {
+                if (n % 2 == 0 && i == n / 2 - 1)
+                {
+                    Console.WriteLine("*" + new String('/', 2 * n - 2) + "*" + new String('|', n) + "*" + new String('/', 2 * n - 2) + "*");
+                }
+
+                else if (n % 2 == 1 && i == Math.Floor(n / 2.0))
+                {
+                    Console.WriteLine("*" + new String('/', 2 * n - 2) + "*" + new String('|', n) + "*" + new String('/', 2 * n - 2) + "*");
+                }
+
+                else
+                {
+
+
+                    Console.WriteLine("*" + new String('/', 2 * n - 2) + "*" + new String(' ', n) + "*" + new String('/', 2 * n - 2) + "*");
+                }
+            }
+
+            Console.WriteLine(new String('*', 2 * n) + new String(' ', n) + new String('*', 2 * n));
+
+        }
+    }
+}
